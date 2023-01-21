8037efc893538083974b0e4e850f999c59e19085
night780 <96319709+night780@users.noreply.github.com>
2023-01-20 21:12:30 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.html'; ?>
+
+
+<div style="display: flex; justify-content: center; padding: 10%";>
+
+    <form action="newPlan.php" method="get">
+        <button type="submit" class="center-button">New Plan</button>
+    </form>
+
+    <form action="updatePlan.php" method="get">
+        <button type="submit" class="center-button">Update Plan</button>
+    </form>
+
+
+</div>
+
+</html>
+</body>
---
+++ newPlan.php
@@ -0,0 +1,39 @@
+<?php include 'includes/header.html'; ?>
+
+<div class="details">
+    <h3>Unique ID:</h3>
+    <p> <?php function IdGenerator($length = 6) {
+            $characters = 'ABCDEFHIJKLMNOPQRSTUVWXYZ2345679';
+            $strlen = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $strlen - 1)];
+            }
+            return $randomString;
+        }
+        echo "Your personal ID: ".IdGenerator(); ?> </p>
+</div>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" onsubmit="return confirm('Are you sure you want to submit this form?');">
+    <div class="form-group">
+        <label for="summerClasses">Summer Classes:</label>
+        <input type="text" class="form-control" id="summerClasses" name="summerClasses">
+    </div>
+
+    <div class="form-group">
+        <label for="fallClasses">Fall Classes:</label>
+        <input type="text" class="form-control" id="fallClasses" name="fallClasses">
+    </div>
+    <div class="form-group">
+        <label for="winterClasses">Winter Classes:</label>
+        <input type="text" class="form-control" id="winterClasses" name="winterClasses">
+    </div>
+    <div class="form-group">
+        <label for="springClasses">Spring Classes:</label>
+        <input type="text" class="form-control" id="springClasses" name="springClasses">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</body>
+</html>
